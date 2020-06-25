130259ceedc0934a57b27d74aeb37831e85cc1d2
Maya Stucky <mayaatstucky@gmail.com>
2020-06-25 17:58:09 -0400

    added questions and wrote file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
+const information = require("./generateMarkdown");
 // array of questions for user
 const questions = [];
 
@@ -8,54 +9,65 @@ function writeToFile(fileName, data) {}
 
 // function to initialize program
 function init() {
-  inquirer.prompt([
-    {
-      type: "input",
-      message: "What is the title of your project?",
-      name: "title",
-    },
-    {
-      type: "input",
-      message: "Please enter a short description of your project.",
-      name: "description",
-    },
-    {
-      type: "input",
-      message: "What are the installation instructions for your project?",
-      name: "instructions",
-    },
-    {
-      type: "input",
-      message: "What is the usage information for your project?",
-      name: "usage",
-    },
-    {
-      type: "input",
-      message: "What are the contribution guidelines for your project?",
-      name: "contribution",
-    },
-    {
-      type: "input",
-      message: "What are the test instructions for your project?",
-      name: "test",
-    },
-    {
-      type: "list",
-      message: "What license is your project under?",
-      name: "license",
-      choices: ["MIT", "GPL", "Mozilla Public License", "Apache License"],
-    },
-    {
-      type: "input",
-      message: "What is your GitHub username?",
-      name: "username",
-    },
-    {
-      type: "input",
-      message: "What is your email address?",
-      name: "email",
-    },
-  ]);
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title",
+      },
+      {
+        type: "input",
+        message: "Please enter a short description of your project.",
+        name: "description",
+      },
+      {
+        type: "input",
+        message: "What are the installation instructions for your project?",
+        name: "instructions",
+      },
+      {
+        type: "input",
+        message: "What is the usage information for your project?",
+        name: "usage",
+      },
+      {
+        type: "input",
+        message: "What are the contribution guidelines for your project?",
+        name: "contribution",
+      },
+      {
+        type: "input",
+        message: "What are the test instructions for your project?",
+        name: "test",
+      },
+      {
+        type: "list",
+        message: "What license is your project under?",
+        name: "license",
+        choices: ["MIT", "GPL", "Mozilla Public License", "Apache License"],
+      },
+      {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "username",
+      },
+      {
+        type: "input",
+        message: "What is your email address?",
+        name: "email",
+      },
+    ])
+    .then(function (data) {
+      fs.writeFile("SampleReadMe.md", "Generated ReadMe:", function (error) {
+        if (error) {
+          throw error;
+        }
+        console.log(
+          "Success! Please migrate to the new file with your generated Read Me"
+        );
+      });
+    });
 }
 
 // function call to initialize program
