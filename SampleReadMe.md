2fe596183c42530a8d140df56a9c9714e03c8583
Maya Stucky <mayaatstucky@gmail.com>
2020-06-25 20:43:04 -0400

    formatted readme and added badge

--- SampleReadMe.md
+++ SampleReadMe.md
@@ -1,1 +1,43 @@
- # fhedjsa
+# hi
+
+[![saythanks](https://img.shields.io/badge/say-thanks-ff69b4.svg)](https://saythanks.io/to/kennethreitz)
+
+## Description
+
+hello
+
+## Table of contents
+
+- [Description](#Description)
+- [Instructions](#Instructions)
+- [Usage](#Usage)
+- [Contributions](#Contributions)
+- [Test](#Test)
+- [License](#License)
+- [Questions](#Questions)
+
+## Instructions
+
+none
+
+## Usage
+
+dont
+
+## Contributions
+
+dont
+
+## Test
+
+test
+
+## License
+
+MIT
+
+## Questions:
+
+All questions can be directed to my
+[GitHub](https://github.com/maya) or my
+[E-mail](maya)
--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,14 +1,32 @@
 // function to generate markdown for README
-// const questions = require("./index");
-
-// function generateMarkdown(data) {
-//   return `# Title
-
-// `;
-// }
 
 function generateMarkdown(data) {
-  return ` # ${data.title}`;
+  return ` # ${data.title}
+  [![saythanks](https://img.shields.io/badge/say-thanks-ff69b4.svg)](https://saythanks.io/to/kennethreitz)
+  ## Description
+  ${data.description}
+  ## Table of contents 
+  [Description](#Description)
+  [Instructions](#Instructions)
+  [Contributions](#Contributions)
+  [Test](#Test)
+  [License](#License)
+  [Questions](#Questions)
+  ## Instructions
+  ${data.instructions}
+  ## Usage
+  ${data.usage}
+  ## Contributions
+  ${data.contribution}
+  ## Test
+  ${data.test}
+  ## License
+  ${data.license}
+  ## Questions: 
+  All questions can be directed to: 
+  [GitHub] (https://github.com/${data.username})
+  [Email] (${data.email})
+  `;
 }
 
 module.exports = generateMarkdown;
